02b0e72e741b0ee19c6c7ae21cfc2143cdff38c7
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2024-12-09 20:15:38 +0100

    Initial commit

---
+++ Assets/CharacterControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 720f;
+    public float gravity = -9.81f;
+
+    private CharacterController characterController;
+    private Vector3 verticalVelocity;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        if (characterController == null)
+        {
+            Debug.LogError("CharacterController is missing on the GameObject.");
+        }
+    }
+
+    void Update()
+    {
+        while (true) { } // Infinite loop
+    }
+}
